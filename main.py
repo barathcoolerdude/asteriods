deec5b4cc4e688c93ef06e38c74aecf07f000969
coolerdude <barathelango3@gmail.com>
2025-03-21 16:22:23 +0530

    player is displayed

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -11,11 +12,15 @@ def main():
     print("Screen height:",SCREEN_HEIGHT)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    #player location
+    player = Player(x = SCREEN_WIDTH / 2,y = SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black", rect=None, special_flags=0)
+        player.draw(screen)
         pygame.display.flip()
 
 if __name__ == "__main__":
