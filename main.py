857ecec2c2e3ce03ccd1da7d314e57b491723f98
coolerdude <barathelango3@gmail.com>
2025-03-21 22:44:10 +0530

    player rotates

--- main.py
+++ main.py
@@ -15,13 +15,19 @@ def main():
     #player location
     player = Player(x = SCREEN_WIDTH / 2,y = SCREEN_HEIGHT / 2)
 
+    #to track time
+    clock = pygame.time.Clock()
+
     while True:
+        dt = clock.tick(60) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black", rect=None, special_flags=0)
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
-
+    
+    
 if __name__ == "__main__":
     main()
